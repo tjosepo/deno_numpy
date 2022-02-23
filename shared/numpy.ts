b57cb9815c2fedfeb4db5374a8fa541b643b3620
tjosepo <tjosepo@users.noreply.github.com>
2022-02-22 21:21:37 -0500

    feat: auto install numpy if missing

--- shared/numpy.ts
+++ shared/numpy.ts
@@ -1,2 +1,15 @@
-import { python } from "../deps.ts";
-export default python.import("numpy");
+import { python, PythonError } from "../deps.ts";
+
+let numpy: any;
+
+try {
+  numpy = python.import("numpy");
+} catch (error: unknown) {
+  if (!(error instanceof PythonError)) throw error;
+  if (error.message !== "No module named 'numpy'") throw error;
+  const p = Deno.run({ cmd: "pip install numpy".split(" ") });
+  await p.status();
+  numpy = python.import("numpy");
+}
+
+export default numpy;
