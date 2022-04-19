5ecfe1a3e66f88017d1ad2a3f58296123532f3b6
tjosepo <tjosepo@users.noreply.github.com>
2022-04-18 21:38:28 -0400

    chore: change order of numpy.arange overloads

---
+++ main.ts
@@ -0,0 +1,11 @@
+import np from "https://deno.land/x/numpy@0.1.0/mod.ts";
+
+const x = np.arange(15, { dtype: np.int64 }).reshape(3, 5);
+np.put(x, np.arange(5, 15, 2), -99);
+console.log(x);
+// ndarray { array: [[  0   1   2   3   4]
+// [-99   6 -99   8 -99]
+// [ 10 -99  12 -99  14]] }
+
+console.log(x.max({ axis: 1 }));
+// ndarray { array: [ 4  8 14] }
